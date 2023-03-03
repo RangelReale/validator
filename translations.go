e17296a652c28de4d202ea8dd79cae5147be18a2
Rangel Reale <rangelspam@gmail.com>
2023-03-03 11:48:21 -0300

    comments

--- translations.go
+++ translations.go
@@ -10,23 +10,30 @@ type TranslationFunc func(ut ut.Translator, fe FieldError) string
 // for a 'ut.Translator' for use within the 'TranslationFunc'
 type RegisterTranslationsFunc func(ut ut.Translator) error
 
+// TranslateValidate is a validator interface to use in translator registration
 type TranslateValidate interface {
 	RegisterTranslation(tag string, trans ut.Translator, registerFn RegisterTranslationsFunc, translationFn TranslationFunc) (err error)
 }
 
+// RegisterDefaultTranslations is the function type that translators should have
 type RegisterDefaultTranslations func(v TranslateValidate, trans ut.Translator) (err error)
 
+// RegisterTranslatorDefaultTranslations registers translations in the Translator without needing
+// a validator instance
 func RegisterTranslatorDefaultTranslations(trans ut.Translator, f RegisterDefaultTranslations) error {
-	return f(&registerOnlyTranslateValidate{}, trans)
+	return f(&translatorTranslateValidate{}, trans)
 }
 
-type registerOnlyTranslateValidate struct {
+type translatorTranslateValidate struct {
 }
 
-func (r registerOnlyTranslateValidate) RegisterTranslation(tag string, trans ut.Translator, registerFn RegisterTranslationsFunc, translationFn TranslationFunc) (err error) {
+func (r translatorTranslateValidate) RegisterTranslation(tag string, trans ut.Translator, registerFn RegisterTranslationsFunc, translationFn TranslationFunc) (err error) {
 	return registerFn(trans)
 }
 
+// RegisterValidatorDefaultTranslations register translations in the validator without affecting
+// the Translator.
+// You must have called "RegisterTranslatorDefaultTranslations" on this translator before this call.
 func RegisterValidatorDefaultTranslations(v TranslateValidate, trans ut.Translator, f RegisterDefaultTranslations) error {
 	return f(&validatorTranslateValidate{v}, trans)
 }
@@ -37,7 +44,6 @@ type validatorTranslateValidate struct {
 
 func (r validatorTranslateValidate) RegisterTranslation(tag string, trans ut.Translator, registerFn RegisterTranslationsFunc, translationFn TranslationFunc) (err error) {
 	return r.v.RegisterTranslation(tag, trans, func(ut ut.Translator) error {
-		return nil
+		return nil // translator registration was already done by RegisterTranslatorDefaultTranslations
 	}, translationFn)
-	return nil
 }
